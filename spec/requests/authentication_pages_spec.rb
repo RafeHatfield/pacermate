6feb16531effe95348f336e2853d8361f3530fd2
Rafe Hatfield <rafe.hatfield@suite101.com>
2012-05-31 16:42:29 -0700

    fixed broken specs for renamed menu items

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -38,7 +38,7 @@ describe "Authentication" do
       
       it { should have_selector('title', text: user.name) }
       it { should have_link('Users',    href: users_path) }
-      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('My Stable', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
